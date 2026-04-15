81d904e150993006a0ac09ee21e065b2a96d1ddb
kthana-bg <kthana7503@gmail.com>
2026-04-16 01:16:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ with col1:
 
     camera_html = """
     <div style="display:flex; justify-content:center;">
-        <video id="video" autoplay playsinline style="width:90%; border-radius:16px;"></video>
+        <video id="video" autoplay playsinline style="width:100%; border-radius:16px;"></video>
     </div>
 
     <script>
